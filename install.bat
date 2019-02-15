e16572496f0337d2896de1d11fe35b51fca8b220
Ivan Milosavljevic (TheJavaGuy) <TheJavaGuy@yandex.com>
2019-02-15 11:18:12 +0100

    ADD: new versions of dependencies and plugins, better install script

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 @echo off
+mvn dependency:purge-local-repository -DmanualInclude="org.thejavaguy.archetype:java8-consoleapp-archetype"
 mvn clean install archetype:update-local-catalog archetype:crawl
--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mvn dependency:purge-local-repository -DmanualInclude="org.thejavaguy.archetype:java8-consoleapp-archetype"
 mvn clean install archetype:update-local-catalog archetype:crawl
